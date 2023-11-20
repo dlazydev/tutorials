9dc35a247776d9abc6a61925b1066c6a1984ab56
dlazydev <146261002+dlazydev@users.noreply.github.com>
2023-11-20 21:43:27 +0100

    Update XmlOptimizedPackaging.java

--- xml-2/src/main/java/com/baeldung/xml/binary/XmlOptimizedPackaging.java
+++ xml-2/src/main/java/com/baeldung/xml/binary/XmlOptimizedPackaging.java
@@ -23,45 +23,27 @@ public class XmlOptimizedPackaging {
         this.binaryData = binaryData;
     }
 
-    public String marshalToXml() {
-        try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.newDocument();
+    public String marshalToXml() throws ParserConfigurationException, TransformerException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.newDocument();
 
-            // Root element
-            Element rootElement = document.createElement("XmlOptimizedPackaging");
-            document.appendChild(rootElement);
+        // Root element
+        Element rootElement = document.createElement("XmlOptimizedPackaging");
+        document.appendChild(rootElement);
 
-            // Binary data element
-            Element binaryDataElement = document.createElement("BinaryData");
-            binaryDataElement.setTextContent(new String(binaryData, "UTF-8")); // Just for illustration
-            rootElement.appendChild(binaryDataElement);
+        // Binary data element
+        Element binaryDataElement = document.createElement("BinaryData");
+        binaryDataElement.setTextContent(new String(binaryData, "UTF-8"));
+        rootElement.appendChild(binaryDataElement);
 
-            // Convert the DOM document to a string
-            StringWriter writer = new StringWriter();
-            StreamResult result = new StreamResult(writer);
-            TransformerFactory tf = TransformerFactory.newInstance();
-            Transformer transformer = tf.newTransformer();
-            transformer.transform(new DOMSource(document), result);
+        // Convert the DOM document to a string
+        StringWriter writer = new StringWriter();
+        StreamResult result = new StreamResult(writer);
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        transformer.transform(new DOMSource(document), result);
 
-            return writer.toString();
-        } catch (ParserConfigurationException | TransformerException | UnsupportedEncodingException e) {
-            e.printStackTrace(); // Handle exceptions appropriately in a real application
-            return null;
-        }
-    }
-
-    public static void main(String[] args) {
-        // Read binary data from a file (for illustration purposes)
-        byte[] binaryData = "TestBinaryData".getBytes();
-
-        // Create an instance of XmlOptimizedPackaging
-        XmlOptimizedPackaging xmlOptimizedPackaging = new XmlOptimizedPackaging(binaryData);
-
-        // Marshal to XML
-        String xmlContent = xmlOptimizedPackaging.marshalToXml();
-
-        // Your application logic here
+        return writer.toString();
     }
 }
